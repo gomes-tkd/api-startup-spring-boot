d4f9729e59dd8a66a3abca9e9f9ea9d64c0af3a3
gomes-tkd <jgomestkd@gmail.com>
2024-10-07 17:16:23 -0300

    project base

---
+++ src/main/java/io/github/gomestkd/apistartupspringboot/ApiStartupSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.gomestkd.apistartupspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStartupSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiStartupSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/gomestkd/apistartupspringboot/ApiStartupSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gomestkd.apistartupspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStartupSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
